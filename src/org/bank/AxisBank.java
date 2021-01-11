5832fd84262f82ab888ee0b963d8bc9b8056c53d
Bhargavith <vuday14@gmail.com>
2021-01-11 17:53:21 +0530

    Task completed

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,26 @@
+package org.bank;
+
+public class AxisBank extends BankInfo {
+	@Override
+	public void deposit() {
+		System.out.println("Axis Bank deposit account 15% interest");
+		}
+		
+	public static void main(String[] args) {
+		BankInfo b=new BankInfo();
+		System.out.println("(Parent)BankInfo deposit() ");
+		System.out.print("*******************************");
+		System.out.print("\n");
+		//b.saving();
+		//b.fixed();
+		b.deposit();
+		System.out.print("\n");
+		AxisBank a=new AxisBank();
+		System.out.println("(Child)AxisBank deposit() Overide");
+		System.out.print("************************************");
+		System.out.print("\n");
+		a.deposit();
+	}
+	
+
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,52 @@
+package org.bank;
+
+
+
+public class BankInfo {
+	public void saving() {
+		System.out.println("Savings account");
+
+	}
+	public void fixed() {
+		System.out.println("Fixed account");
+
+	}
+	public void deposit() {
+		System.out.println("BankInfo Deposit account 10% intrest");
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+	public static void main(String[] args) {
+		BankInfo l=new BankInfo();
+		System.out.println("BankInfo Details(Parent Class)");
+		System.out.println("***********************");
+		l.deposit();
+		System.out.println("\n");
+		System.out.println("Axis Bank Details(Child)");
+		System.out.println("***********************");
+	    l.saving();
+		System.out.println("\n");
+	    l.fixed();
+	
+
+	}
+}*/
